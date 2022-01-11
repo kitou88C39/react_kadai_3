a9f4d0b816653f6d5773666284d1fc529c23f5d4
kitou88C39 <kanfutrooper@icloud.com>
2022-01-11 20:27:30 +0900

    fifth commit

--- src/index.js
+++ src/index.js
@@ -4,29 +4,25 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
 import { AuthProvider } from "./auth/AuthProvider";
-//import PrivateRoute from "./auth/PrivateRoute";
+//import { PrivateRoute } from "./auth/PrivateRoute";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProvider>
-        <Router>
-          <Route exact path="/" component={App} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-        </Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </AuthProvider>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
