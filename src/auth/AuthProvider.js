f0d8bf57131b36fd8be0cb7e0c238886da385b49
kitou88C39 <kanfutrooper@icloud.com>
2022-01-14 11:40:11 +0900

    eleven commit

--- src/auth/AuthProvider.js
+++ src/auth/AuthProvider.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../firebase";
+//import { useNavigate } from "react-router-dom";
 
 const AuthContext = React.createContext();
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-
+  //const navigate = useNavigate();
   //サインアップ後認証情報を更新
-  const signup = async (name, email, password, history) => {
+  //const signup = async (name, email, password, history) => {
+  const signup = async (name, email, password, navigate) => {
     try {
       await auth.createUserWithEmailAndPassword(name, email, password);
       auth.onAuthStateChanged((user) => setCurrentUser(user));
-      history.push("/");
+      //history.push("/");
+      navigate("/");
     } catch (error) {
       alert(error);
     }
   };
 
   //ログインさせる
-  const login = async (email, password, history) => {
+  //const login = async (email, password, history) => {
+  const login = async (email, password, navigate) => {
     try {
       await auth.signInWithEmailAndPassword(email, password);
       auth.onAuthStateChanged((user) => setCurrentUser(user));
-      history.push("/");
+      //history.push("/");
+      navigate("/");
     } catch (error) {
       alert(error);
     }
